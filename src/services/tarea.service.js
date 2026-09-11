7441ec9a35ade72cfd141ecfb5ddbdd96958ab22
Alan007 <ramironatty29@gmail.com>
2026-09-11 08:04:40 -0400

    TDLB-5: añadir la funcion getAll, create, update, delete a tarea.service.js

--- src/services/tarea.service.js
+++ src/services/tarea.service.js
@@ -1,14 +1,31 @@
 import { API_URL } from './api';
 
 export async function getAll() {
-
+  const res = await fetch(`${API_URL}/tareas`);
+  return res.json();
 }
 
 export async function create(datos) {
+  const res = await fetch(`${API_URL}/tareas`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(datos),
+  });
+  return res.json();
 }
 
 export async function update(id, datos) {
+  const res = await fetch(`${API_URL}/tareas/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(datos),
+  });
+  return res.json();
 }
 
 export async function deleteTask(id) {
+  const res = await fetch(`${API_URL}/tareas/${id}`, {
+    method: 'DELETE',
+  });
+  return res.json();
 }
