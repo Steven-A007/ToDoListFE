2c892da26edb0ee351a1d524fe915e82d96541fe
Steven-A007 <ramironatty29@gmail.com>
2026-09-14 00:14:11 -0400

    TDLB-6: importar getAll en App.jsx y mostrar respuesta en consola (#6)
    * TDLB-3: crear carpeta services
    
    * TDLB-4: crear tarea.service.js
    
    * TDLB-5: añadir la funcion getAll, create, update, delete a tarea.service.js
    
    * TDLB-6: importar getAll en App.jsx y mostrar respuesta en consola

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import { useEffect } from 'react';
+import { getAll } from './services/tarea.service';
 
 function App() {
+  useEffect(() => {
+    getAll()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error al obtener tareas:', error);
+      });
+  }, []);
 
   return (
-    <>
-      <h1>HELLO WORLD</h1>
-    </>
-  )
+    <h1>Hola Mundo</h1>
+  );
 }
 
-export default App
+export default App;
