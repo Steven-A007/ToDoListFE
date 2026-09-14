d025a832276d8dbc5eb98ac0982ccf6154f2a87a
Alan007 <ramironatty29@gmail.com>
2026-09-14 13:43:25 -0400

    TDLB-18: interceptor (fetch)

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,21 @@
 export const API_URL = 'http://localhost:8000/api';
+
+export async function apiFetch(endpoint, options = {}) {
+  const token = localStorage.getItem('token');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    ...(options.headers || {}),
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers,
+  });
+
+  return res;
+}
