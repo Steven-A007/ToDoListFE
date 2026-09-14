ed6e3000eb4f19457100007b5c5a4b3c1b584591
Steven-A007 <ramironatty29@gmail.com>
2026-09-13 20:28:35 -0400

    TDLB-4: crear tarea.service.js (#4)
    * TDLB-3: crear carpeta services
    
    * TDLB-4: crear tarea.service.js

---
+++ src/services/tarea.service.js
@@ -0,0 +1,14 @@
+import { API_URL } from './api';
+
+export async function getAll() {
+
+}
+
+export async function create(datos) {
+}
+
+export async function update(id, datos) {
+}
+
+export async function deleteTask(id) {
+}
